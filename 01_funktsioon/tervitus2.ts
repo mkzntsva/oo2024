39889de835922a58a977938bde14cd43d9359e8b
mkzntsva <146323632+mkzntsva@users.noreply.github.com>
2024-02-05 15:02:51 +0200

    Add files via upload

---
+++ 01_funktsioon/tervitus2.ts
@@ -0,0 +1,19 @@
+let eesnimi:string="Juku";
+let kogus:number=5;
+for(let i:number=0; i<kogus; i++){
+  console.log("Tere, "+eesnimi+" "+i+". korda"); 
+}
+
+//Trükkige välja arvude ruudud ühest kahekümneni
+//1 4 9 16 25
+
+for(let i:number=1; i<=20; i++){
+  console.log(i*i);
+}
+
+//Trükkige välja muutujatega ette antud suuruses
+//korrutustabel
+
+//process.stdout.write("a");
+//process.stdout.write("b/n");
+//process.stdout.write("a");
